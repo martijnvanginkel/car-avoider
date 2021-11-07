d96aa62b434340a733c32d83472f082dc3252037
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2021-11-07 18:30:00 +0100

    feat: added headlights and finished rooftop

--- src/Car.tsx
+++ src/Car.tsx
@@ -5,12 +5,15 @@ function Car() {
         <div className="BaseLayer">
             <div className="WindowLayer">
                 <div className="RoofContainerLayer">
-                    <div className="RoofLine">
-                    </div>
-                    <div className="RoofLayer">
-                    </div>
+                    <div className="RoofLineTopLeft"></div>
+                    <div className="RoofLineTopRight"></div>
+                    <div className="RoofLineBottomLeft"></div>
+                    <div className="RoofLineBottomRight"></div>
+                    <div className="RoofLayer"></div>
                 </div>
             </div>
+            <div className="HeadLightLeft"></div>
+            <div className="HeadLightRight"></div>
         </div>
     )
 }
