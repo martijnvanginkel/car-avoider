499cee9a114b48a08c49789e6909c34424f530ca
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2021-11-25 23:01:53 +0100

    refactor: got working version of setTimeout useeffect working for one lane

--- src/useTrafficSpawner.ts
+++ src/useTrafficSpawner.ts
@@ -19,6 +19,7 @@ interface Lanes {
 
 interface LaneStatus {
     occupied: boolean
+    speed: number
 }
 
 function useTrafficSpawner() {
@@ -26,22 +27,32 @@ function useTrafficSpawner() {
     const [trafficObjects, setTrafficObjects] = useState<TrafficObject[]>(initialTrafficState)
     const trafficRef = useRef(initialTrafficState)
 
-    const [lanes, setLanes] = useState<Lanes>({
-        [Position.left]: { occupied: false },
-        [Position.center]: { occupied: false },
-        [Position.right]: { occupied: false }
+    const initialLanesState: Lanes = {
+        [Position.left]: { occupied: false, speed: 0 },
+        [Position.center]: { occupied: false, speed: 0 },
+        [Position.right]: { occupied: false, speed: 0 }
+    }
+    const [lanes, setLanes] = useState<Lanes>(initialLanesState)
+    const lanesRef = useRef(initialLanesState)
+
+    const [spawnHistory, setSpawnHistory] = useState<any>({
+        [Position.left]: { speed: 0 },
+        [Position.center]: { speed: 0 },
+        [Position.right]: { speed: 0 }
     })
+
+    const [lastSpawn, setLastSpawn] = useState<any>({ [Position.left]: 0, [Position.right]: 1 })
 
     const isGameOver = useGameOver()
     const position: Position = usePlayerPosition() 
 
     function getRandomSpawnTime() {
-        const randomNumber = Math.floor(Math.random() * 6) + 4
+        const randomNumber = Math.floor(Math.random() * 4) + 2
         return randomNumber * 1000
     }
 
     function getRandomSpeed() {
-        return Math.floor(Math.random() * 11) + 9 
+        return Math.floor(Math.random() * 11) + 7 
     }
 
     useEffect(() => {
@@ -49,41 +60,92 @@ function useTrafficSpawner() {
     })
 
     useEffect(() => {
-        if (isGameOver) {
-            return
+
+        const getSpeedNotSameAsLast = () => {
+            let speed = getRandomSpeed()
+            while (speed === lastSpawn[Position.left].speed) {
+                speed = getRandomSpeed()
+            }
+            return speed
         }
 
-        const interval = setInterval(() => {
-            const { positionOne, positionTwo } = getTwoRandomPositions()
-            const id1 = getUniqueId()
-            const id2 = getUniqueId()
+        const speed = getSpeedNotSameAsLast() 
+        console.log(speed)
 
-            const object1 = {
-                id: id1,
-                position: positionOne,
-                speed: getRandomSpeed() 
-            }
-
-            const object2 = {
-                id: id2,
-                position: positionTwo,
-                speed: getRandomSpeed() 
-            }
-
+        const id = getUniqueId()
+        console.log('use effect')
+        const timer = setTimeout(() => {
             setTrafficObjects([...trafficRef.current,
-                object1,
-                object2
+                {
+                    id: id, 
+                    position: Position.left,
+                    speed: 5 
+                }
             ])
 
-        }, getRandomSpawnTime())
-        return () => clearInterval(interval)
-    }, [isGameOver])
+            setLastSpawn({ [Position.left]: speed, [Position.right]: lastSpawn[Position.right] })
+           
+        }, 5000) 
+
+
+        return () => clearTimeout(timer)
+        
+    }, [lastSpawn[Position.left]])
+
+    // have an useEffect for every Lane, then have it all have its own setTimeout and it will calculate it's own timeout based on what it previously was if its set at all
+
+//    useEffect(() => {
+//        if (isGameOver) {
+//            return
+//        }
+//
+//        // als speed 10 is van de eerste en speed 5 van de tweede, dan hitten ze elkaar in het midden van de lane
+//        // wanneer de eerste speed 10 is, moet de tweede met speed 5 minimaal 5 wachten
+//        // wanneer de eerste speed 11 is en de tweede speed 8, moet de tweede minimaal 3 wachten
+//        // dus vorige speed - huidige speed = minimale spawn tijd
+//        const { positionOne, positionTwo } = getTwoRandomPositions()
+//
+//        const timer = setTimeout(() => {
+//            const id1 = getUniqueId()
+//            const id2 = getUniqueId()
+//
+//            const object1 = {
+//                id: id1,
+//                position: Position.left,
+//                speed: 10 
+//            }
+//
+//            const object2 = {
+//                id: id2,
+//                position: Position.left,
+//                speed: 6 
+//            }
+//
+//            setTrafficObjects([...trafficRef.current,
+//                object1,
+//                object2
+//            ])
+//
+//            setSpawnHistory(prev => prev + 1)
+//
+//        }, 5000) // getRandomSpawnTime
+//        return () => clearTimeout(timer)
+//    }, [spawnHistory, isGameOver])
+    //
+    function setLaneSpeed(lane: Position, speed: number) {
+        setLanes(prevState => {
+            return {
+                ...prevState,
+                [Position[lane]]: { occupied: lanes[Position[lane]].occupied, speed: speed } 
+            }
+        })
+    }
 
     function setLaneOccupied(lane: Position, occupied: boolean) {
         setLanes(prevState => {
             return {
                 ...prevState,
-                [Position[lane]]: { occupied: occupied } 
+                [Position[lane]]: { occupied: occupied, speed: lanes[Position[lane]].speed } 
             }
         })
     }
